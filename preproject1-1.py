151de123f42f55f66d892dadf3303f306a4c3d57
ypkoo <ypkoo93@gmail.com>
2015-09-22 21:55:27 +0900

    initial implementation of saving config

--- preproject1-1.py
+++ preproject1-1.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 
 from __future__ import print_function
+import os
+import xml.etree.ElementTree as ET
 
 __author__ = 'koo'
 
@@ -12,7 +14,7 @@ class Automata:
         self.init_state = None
         self.final_states = {}
         
-    def __str__(self):
+    def get_name(self):
         return self.name
 
     def set_voca(self, voca):
@@ -31,6 +33,9 @@ class Automata:
 
     def get_init_state(self):
         return self.init_state
+
+    def get_final_states(self):
+        return self.final_states.values()
 
     # get State object by state name
     def get_state(self, state_name):
@@ -87,6 +92,8 @@ class Automata:
             # add check input and next have proper type.
             self.trans_func.append([input, next])
 
+        def get_trans_func(self):
+            return self.trans_func
 
         def trans(self, input):
             for func in self.trans_func:
@@ -94,8 +101,63 @@ class Automata:
                     return func[1]
             return None
 
+# indent function for formatting xml. (from https://wikidocs.net/42)
+def indent(elem, level=0):
+    i = "\n" + level*"  "
+    if len(elem):
+        if not elem.text or not elem.text.strip():
+            elem.text = i + "  "
+        if not elem.tail or not elem.tail.strip():
+            elem.tail = i
+        for elem in elem:
+            indent(elem, level+1)
+        if not elem.tail or not elem.tail.strip():
+            elem.tail = i
+    else:
+        if level and (not elem.tail or not elem.tail.strip()):
+            elem.tail = i
 
+# save current automata to use later
+def save_automata(automata):
+    dir = "configs/"
+    filename = dir + automata.get_name() + ".xml"
 
+    if not os.path.exists(dir):
+        os.makedirs(dir)
+
+    root = ET.Element("automata")
+
+    automata_name = ET.SubElement(root, "name")
+    automata_name.text = automata.get_name()
+
+    voca = ET.SubElement(root, "voca")
+    for symbol in automata.get_voca():
+        ET.SubElement(voca, "symbol").text = symbol
+
+    init_state = ET.SubElement(root, "init_state")
+    init_state.text = automata.get_init_state().get_name()
+
+    final_states = ET.SubElement(root, "final_states")
+    for state in automata.get_final_states():
+        ET.SubElement(final_states, "state").text = state.get_name()
+
+    states = ET.SubElement(root, "states")
+    for state in automata.get_all_states():
+        s = ET.SubElement(states, "state")
+        ET.SubElement(s, "name").text = state.get_name()
+
+        trans_func = ET.SubElement(s, "trans_func")
+
+        tf = state.get_trans_func()
+        for pair in tf:
+            func = ET.SubElement(trans_func, "func")
+            ET.SubElement(func, "input").text = pair[0]
+            ET.SubElement(func, "next").text = pair[1].get_name()
+
+    indent(root)
+    ET.dump(root)
+
+    ET.ElementTree(root).write(filename)
 
 def make_dfa():
 
@@ -142,6 +204,8 @@ def make_dfa():
     automata.show_all_states()
     automata.show_voca()
 
+    save_automata(automata)
+
     while True:
 
         input_string = raw_input("input string? ")
@@ -151,8 +215,6 @@ def make_dfa():
             print("string accepted.")
         else:
             print("string not accepted.")
-
-
 
 
 make_dfa()
